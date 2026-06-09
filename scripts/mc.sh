bc602a23c18ef78f457af2e0bbe8fed6873d3a86
Your Name <you@example.com>
2026-06-09 10:12:25 +0400

    fix

--- scripts/mc.sh
+++ scripts/mc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+mc
 mkdir ~/.local/share/mc/skins -p && cd ~/.local/share/mc/skins && wget https://raw.githubusercontent.com/soko1/dotfiles/refs/heads/master/.local/share/mc/skins/electricblue256.ini && sed -i s/default/electricblue256/ ~/.config/mc/ini
